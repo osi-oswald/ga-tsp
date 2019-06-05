b5b7d70a0b37b2d06289e3d9f0817892f4c73cb4
Patrick Oswald <patrick.oswald@imnoo.com>
2019-06-05 23:48:25 +0200

    added prettier with git hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import cn from './App.module.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={cn.host}>
+      <header className={cn.header}>
+        <img src={logo} className={cn.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={cn.link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
