cce1103f2b7182832d6dc62952d33b53425b4613
Patrick Oswald <patrick.oswald@imnoo.com>
2019-06-10 19:28:44 +0200

    uploaded github page

---
+++ docs/precache-manifest.fe7110c6625133279f2c697782486edb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90699ad56d4e542949b9ebdb44600835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec653ddf06b9cf08f782",
+    "url": "/static/css/main.a0ba1d0f.chunk.css"
+  },
+  {
+    "revision": "78cabeaf9e83d363af86",
+    "url": "/static/js/2.61402a81.chunk.js"
+  },
+  {
+    "revision": "ec653ddf06b9cf08f782",
+    "url": "/static/js/main.8a1ad12c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
